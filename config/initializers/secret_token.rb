1eed8c5b3c2e0c8499b7a0ebf54ebe0c5728e10b
luis gutierrez <lugtlugt@gmail.com>
2012-07-10 14:04:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PACCfdi::Application.config.secret_token = '8a9077a21b752366906c6a3e5680a3995c2b434264f030a08206da1103f099a17f522d5551092d7bc3da45864982f39dd3c3c57ecb701fdd638c711e6ae9de64'
